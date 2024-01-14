36a62231d11e68894614c7b9e2d546a47a661e0f
Mihails Popovs <mp@best.lv>
2024-01-14 16:51:34 +0200

    Use Cloudfare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html',
-			precompress: true
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/my-fitness-appointment' : ''
-		},
+		adapter: adapter(),
 		alias: {
 			$components: './src/components',
 			$helpers: './src/helpers',
